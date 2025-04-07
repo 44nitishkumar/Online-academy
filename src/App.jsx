db03053c3c30d366244386bcf5d00b3a3b30a0f3
Nitish Kumar <44nitishkumar@gmail.com>
2025-04-07 17:21:08 +0530

    Navbar address changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import Login from "./components/Login";
